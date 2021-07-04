dac182cfc43a2c50c4a4dbfe28b027a681e9598b
Troy Chryssos <troychryssos@gmail.com>
2021-07-04 11:31:20 -0400

    Initial commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import { Flex } from 'rebass';
+import Layout from '~/components/meta/Layout';
+
+const Home: React.FC = () => (
+  <Layout>
+    <Flex alignItems="center" justifyItems="center">
+      Welcome to Next JS! Edit src/pages/Home.tsx to get started...
+    </Flex>
+  </Layout>
+);
+
+export default Home;
