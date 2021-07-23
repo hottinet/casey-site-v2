94348d83e5d93a2acfac4e34127c2a48ff0209f7
Casey Bradford <caseyebradford@gmail.com>
2021-07-23 17:50:25 -0400

    password overlay styled

--- src/components/meta/AuthOverlay.tsx
+++ src/components/meta/AuthOverlay.tsx
@@ -1,29 +1,62 @@
 import styled from '@emotion/styled';
 import { ChangeEventHandler, useContext, useState } from 'react';
 
+import Body from '~/components/typography/Body';
 import Heading from '~/components/typography/Heading';
 import { AuthContext } from '~/contexts/authContext';
 
 import Box from '../box/Box';
-import FlexBox from '../box/FlexBox';
-import TextButton from '../buttons/TextButton';
+import IconButton from '../buttons/IconButton';
+import Pickle from '../pickles/Pickle';
 
-const Input = styled.input<{ isWrong?: boolean }>`
-  height: ${({ theme }) => theme.spacing[48]};
-  width: 33%;
+const Input = styled.input`
+  background-color: transparent;
+  height: ${({ theme }) => theme.spacing[80]};
   min-width: ${({ theme }) => theme.spacing[128]};
-  font-size: ${({ theme }) => theme.fontSize.heading};
-  margin-bottom: ${({ theme }) => theme.spacing[16]};
+  font-size: 100px;
   padding: ${({ theme }) => theme.spacing[8]};
-  border-color: ${({ theme, isWrong }) => isWrong && theme.colors.red};
-  border-radius: ${({ theme }) => theme.spacing[4]};
+  outline: none;
+  border: none;
+  border-bottom: 2px solid white;
+  color: white;
+  width: 100%;
+  ::placeholder {
+    color: white;
+    opacity: 50%;
+  }
 `;
 
-const AuthOverlay: React.FC = () => {
-  const { setIsAuthorized } = useContext(AuthContext);
+const PwIconButton = styled(IconButton)`
+  width: 100px;
+  height: 100px;
+  flex-shrink: 0;
+  margin-left: ${({ theme }) => theme.spacing[24]};
+`;
 
+const PwHeading = styled(Heading)`
+  margin-left: ${({ theme }) => theme.spacing[24]};
+`;
+
+const ButtonBody = styled(Body)`
+  color: white;
+`;
+
+const PwPickle = styled(Pickle)<{ isWrong?: boolean }>`
+  background-color: ${({ theme, isWrong }) =>
+    isWrong ? theme.colors.red : theme.colors.green};
+  width: 94%;
+  padding: ${({ theme }) => theme.spacing[24]};
+  border: none;
+  justify-content: space-between;
+`;
+
+type PasswordInputProps = {
+  setIsWrong: (isWrong: boolean) => void;
+};
+
+const PasswordInput: React.FC<PasswordInputProps> = ({ setIsWrong }) => {
+  const { setIsAuthorized } = useContext(AuthContext);
   const [password, setPassword] = useState('');
-  const [isWrong, setIsWrong] = useState(false);
 
   const onChange: ChangeEventHandler<HTMLInputElement> = (e) =>
     setPassword(e.target.value);
@@ -37,19 +70,32 @@ const AuthOverlay: React.FC = () => {
     }
     setPassword('');
   };
+  return (
+    <>
+      <Input
+        key="password"
+        placeholder="Password"
+        type="password"
+        value={password}
+        onChange={onChange}
+      />
+      <PwIconButton type="submit" variant="secondary" onClick={onSubmit}>
+        <ButtonBody bold mb={0}>
+          Submit
+        </ButtonBody>
+      </PwIconButton>
+    </>
+  );
+};
 
+const AuthOverlay: React.FC = () => {
+  const [isWrong, setIsWrong] = useState(false);
   return (
     <Box>
-      <FlexBox center column mb={80}>
-        <Heading>A password is required to view this page</Heading>
-        <Input
-          isWrong={isWrong}
-          type="password"
-          value={password}
-          onChange={onChange}
-        />
-        <TextButton label="Submit" type="submit" onClick={onSubmit} />
-      </FlexBox>
+      <PwHeading>A password is required to view this page</PwHeading>
+      <PwPickle isWrong={isWrong}>
+        <PasswordInput setIsWrong={setIsWrong} />
+      </PwPickle>
     </Box>
   );
 };
