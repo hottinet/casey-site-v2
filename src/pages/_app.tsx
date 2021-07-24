cdfa862988296e75b0ed3661893542de7b21de74
Troy Chryssos <troychryssos@gmail.com>
2021-07-24 19:49:42 -0400

    casey cursor

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -56,6 +56,9 @@ const globalStyles = css`
 const GlobalWrapper = styled(FlexBox)`
   width: 100%;
   overflow: hidden;
+  ${theme.breakpoints.sm} {
+    cursor: url('/Misc/cursor.png'), auto;
+  }
 `;
 
 const HoverTarget = styled.div`
