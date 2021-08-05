2520b6580479ca867b59ca873c745cdf866a8809
Troy Chryssos <troy@codecademy.com>
2021-08-05 09:30:35 -0400

    cleaning up textbutton styles

--- src/components/buttons/TextButton.tsx
+++ src/components/buttons/TextButton.tsx
@@ -17,38 +17,37 @@ const AnimationWrapper = styled.div`
   margin: ${({ theme }) => theme.spacing[4]};
 `;
 
-const StyledButton = styled(Button)<Pick<TextButtonProps, 'forceHover'>>`
-  height: ${({ theme }) => theme.spacing[48]};
-  padding: ${({ theme }) => theme.spacing[12]};
-  min-width: 9.875rem;
-  border-radius: 500px;
-  border: ${({ theme, variant }) =>
-    `solid 3px ${
+const StyledButton = styled(Button)<Pick<TextButtonProps, 'forceHover'>>(
+  ({ theme, variant, forceHover }) => ({
+    height: theme.spacing[48],
+    padding: theme.spacing[12],
+    minWidth: '9.875rem',
+    borderRadius: 500,
+    border: `solid 3px ${
       variant === 'primary' ? theme.colors.text : theme.colors.textSecondary
-    }`};
-  ::before {
-    content: ' ';
-    position: absolute;
-    top: -0.1rem;
-    left: -0.1rem;
-    right: -0.1rem;
-    bottom: -0.1rem;
-    border: ${({ theme, variant }) =>
-      `1px solid ${
+    }`,
+    '::before': {
+      content: '" "',
+      position: 'absolute',
+      top: '-0.1rem',
+      left: '-0.1rem',
+      right: '-0.1rem',
+      bottom: '-0.1rem',
+      border: `1px solid ${
         variant === 'primary' ? theme.colors.text : theme.colors.textSecondary
-      }`};
-    border-radius: 60px;
-    transform-origin: center center;
-    transform: ${({ forceHover }) =>
-      forceHover ? 'scale(1)' : 'scale(0.91, 0.75)'};
-    transition: transform 0.2s linear;
-  }
-  &:hover {
-    &:before {
-      transform: scale(1);
-    }
-  }
-`;
+      }`,
+      borderRadius: 60,
+      transformOrigin: 'center center',
+      transform: forceHover ? 'scale(1)' : 'scale(0.91, 0.75)',
+      transition: 'transform 0.2s linear',
+    },
+    '&:hover': {
+      '&:before': {
+        transform: 'scale(1)',
+      },
+    },
+  })
+);
 
 const StyledText = styled(Body)<Pick<TextButtonProps, 'variant'>>`
   color: ${({ theme, variant }) =>
