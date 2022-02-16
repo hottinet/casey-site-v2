95cfa5cd507f4c7e139fcddbdfc20d2698662e5b
Casey Bradford <caseyebradford@gmail.com>
2022-02-16 11:42:07 -0500

    paper page fill out

--- src/pages/zola-paper.tsx
+++ src/pages/zola-paper.tsx
@@ -18,54 +18,142 @@ const ZolaPaper: React.FC = () => (
     </ContentBox>
     <DetailPickle>
       <StackedText
-        bottomText="Increase conversion on invites and save the dates in the Zola app."
+        bottomText="Increase conversion on the order of samples and the purchasing of ßinvites and save the dates in the Zola app."
         topText="The Goal"
       />
       <StackedText
-        bottomText="Work on several parts on the paper experience to increase conversion on ordering samples and paper purchases"
+        bottomText="Revisit several parts of the paper experience to increase conversion on ordering samples and paper purchases"
         topText="My Role"
       />
     </DetailPickle>
     <ContentBox column>
       <Title bold>Increasing traffic to the samples flow</Title>
-      <Heading>Changing Order a Proof to Order a Sample</Heading>
-      <Heading>Use universal photos in promos if available</Heading>
-      <Heading>Reinforce FREE messaging</Heading>
+      <Body mb={20}>
+        In the Zola app, couples can order a group of free samples or a free
+        proof of their paper order. We know that couples who order one or the
+        other convert to placing paper orders at a high rate. However, there is
+        not a clear distinction between how we expect couples to use the two
+        options.{' '}
+      </Body>
+      <Body mb={20}>
+        Our hypothesis is that the if we prioritize adding entry points to
+        samples over proofs, especially for couples early in their paper
+        journey, we can increase conversion because the samples flow is simpler
+        and quicker to complete and also the word &quot;Sample&quot; is a little
+        more user friendly.
+      </Body>
+      <GridBox center>
+        <Box>
+          <Heading>
+            Order a <s>Proof</s> Sample
+          </Heading>
+          <Body>
+            On PDPs for paper where samples were available, we changed our
+            secondary CTA from &quot;Order a Proof&quot; to &quot;Order a
+            Sample&quot;. We thought that this language was more straightforward
+            to the user and that driving couples into our samples flow would be
+            a better experience.
+          </Body>
+        </Box>
+        <Image
+          alt="Before and After changing the CTA"
+          src="/ZolaPaper/zola-paper-sample-cta.png"
+        />
+      </GridBox>
+      <GridBox center>
+        <Box>
+          <Heading>Personalizing our promos</Heading>
+          <Body>
+            There are many places in the Zola experience for couples to upload
+            photos including their wedding website, registry and other wedding
+            communications. Because Zola stores the main photos, we are able to
+            show couples previews of paper products including photos they have
+            uploaded instead of stock photography. Being able to envision
+            themselves in the product makes couples more excited to order
+            samples.
+          </Body>
+        </Box>
+        <Image
+          alt="Diagram showing photo being placed in promo"
+          src="/ZolaPaper/zola-paper-photo-promo.png"
+        />
+      </GridBox>
+
+      <Heading>Reinforce FREE messaging / More Entrypoints</Heading>
       <Body>
         Drafts page coupon code and free message on plp, push notification
       </Body>
-      <Title bold>Improve the Samples Flow</Title>
+      <Title bold>Improving Conversion in the Samples Flow</Title>
+      <Body>
+        Samples are a very important for getting users to purchase Save the
+        Dates and Invitations. We noticed that in the native app our samples
+        flow had a much lower completion rate than on web and many users were
+        dropping off in the first few steps. Becuase a high volume of samples
+        orders are placed in the app, it was important for us to remove friction
+        in this process. However, large changes to the functionality would not
+        be in scope.
+      </Body>
       <GridBox>
         <Box>
           <Heading bold>Browsing with intent</Heading>
           <ul>
             <li>
               <Body mb={20}>
-                Introduce filtering so that couples can find exactly what
-                they&apos;re looking for
+                We introduce filtering on the card selection page so that
+                couples could find exactly what they&apos;re looking for
               </Body>
             </li>
             <li>
               <Body mb={20}>
-                Allow the images to be viewed full screen so that couples can
-                see designs better before commiting
+                We created a way for users to view the card images in full
+                screen so that couples could carefully inspect designs before
+                adding to their order.
               </Body>
             </li>
             <li>
               <Body mb={20}>
-                Reiterate &quot;FREE&quot; messaging in the browsing experience
+                We also reiterated the &quot;FREE&quot; messaging within the
+                browsing experience to increase confidence.
               </Body>
             </li>
           </ul>
         </Box>
         <Heading>Video of browse flow</Heading>
       </GridBox>
-      <Heading bold>Customization</Heading>
-      <Body>photo first step and universal photo flow</Body>
-      <Title bold>Faster Review to Checkout</Title>
-      <Body>skip cart step</Body>
-      <Body>add discount code and free shipping message above fold</Body>
+      <Heading bold>
+        Introducing Universal Photos in the Customization Flow
+      </Heading>
+      <Body mb={20}>
+        While we did not want to make any large changes to the samples
+        customization flow, we decided that reordering the photo upload step to
+        be the first step might make couples more excited about the process. We
+        know that our couples love to see themselves reflected in our products
+        as much as possible.
+      </Body>
+      <Body>
+        To make this step more seamless for couples who might have uploaded
+        photos to other experiences in on Zola, we were able to pull in and
+        auto-select these photos. Our hypothesis was that this would save
+        couples the trouble of fumbling through their photo library for the
+        perfect pic when we might already have access to their number one.
+      </Body>
+      <Heading bold> video of customization </Heading>
+      <Title bold>Speeding up Paper Checkout</Title>
+      <Body>
+        One of the big differences between the web and native experiences was
+        that on web, we took users from the Review step straight to Checkout,
+        skipping the Cart.
+      </Body>
+      <Body>
+        In the web experience there is also much more real estate on the screen
+        to add messaging about coupon codes and free shipping in the review
+        step.
+      </Body>
       <Title bold>Results!</Title>
+      <Body>
+        Our goal was to increase annualized revenue by 170K and we found that
+        with these small changes would see an estimated increase of 350K.
+      </Body>
     </ContentBox>
   </Layout>
 );
