619f68bae099fc549b6a540444ded02561393777
Troy Chryssos <troychryssos@gmail.com>
2024-02-18 16:43:47 -0800

    spinning arrow button

--- src/components/buttons/NextProjectNavButton.tsx
+++ src/components/buttons/NextProjectNavButton.tsx
@@ -1,14 +1,13 @@
+import { keyframes } from '@emotion/react';
 import styled from '@emotion/styled';
 import { useState } from 'react';
 
 import { NAV_BAR_HEIGHT } from '~/constants/styles';
 
-import { Box } from '../box/Box';
 import { FlexBox } from '../box/FlexBox';
 import Arrow from '../icons/Arrow';
 import { Link } from '../Link';
 import { Text } from '../typography/Text';
-import { Animator, makeSharedRotatingButtonStyles } from './RotatingIconButton';
 
 const NextProjectLink = styled(Link)`
   position: absolute;
@@ -18,14 +17,20 @@ const NextProjectLink = styled(Link)`
   background-color: ${({ theme }) => theme.colors.blue};
 `;
 
-const ArrowWrapper = styled(Box)<{ hovered: boolean }>(({ theme, hovered }) =>
-  makeSharedRotatingButtonStyles({
-    theme,
-    color: 'textSecondary',
-    forceHover: hovered,
-    borderWidth: 2,
-  })
-);
+const spin = keyframes`
+  from {
+    transform: rotate3D(0, 1, 0, 0deg);
+  }
+  to {
+    transform: rotate3D(0, 1, 0, 360deg);
+  }
+`;
+
+const Animator = styled(FlexBox)<{ hovered: boolean }>(({ hovered }) => ({
+  ...(hovered && {
+    animation: `${spin} 2s linear infinite`,
+  }),
+}));
 
 interface NextProjectButtonProps {
   nextPageHref?: string;
@@ -48,11 +53,19 @@ export function NextProjectButton({ nextPageHref }: NextProjectButtonProps) {
       }}
     >
       <FlexBox alignItems="center" gap={12} height="100%" paddingX={32}>
-        <ArrowWrapper height={20} hovered={hovered} width={20}>
-          <Animator borderRadius="round" center>
-            <Arrow height={12} title="" titleId="" width={12} />
-          </Animator>
-        </ArrowWrapper>
+        <Animator
+          alignItems="center"
+          borderColor="textSecondary"
+          borderRadius="round"
+          borderStyle="solid"
+          borderWidth={2}
+          height={20}
+          hovered={hovered}
+          justifyContent="center"
+          width={20}
+        >
+          <Arrow height={12} title="" titleId="" width={12} />
+        </Animator>
         <Text color="textSecondary" variant="subtitle3">
           Next&nbsp;Project
         </Text>
