8256667bacba283f34863923eef8e68713101759
Troy Chryssos <troychryssos@gmail.com>
2024-02-23 19:17:10 -0800

    whoops we needed that container padding util

--- src/components/ContentContainer.tsx
+++ src/components/ContentContainer.tsx
@@ -1,23 +1,38 @@
 import { useTheme } from '@emotion/react';
 
+import { Spacing } from '~/typings/theme';
+import { useSizeByBreakpoint } from '~/utils/useBreakpoints';
+
 import { FlexBox, FlexBoxProps } from './box/FlexBox';
 
 interface ContentContainerProps extends FlexBoxProps {
   metaPage?: boolean;
 }
 
+export const useContentContainerPadding = (
+  metaPage?: ContentContainerProps['metaPage']
+): Spacing => {
+  const sizes = {
+    base: 20,
+    sm: 64,
+    md: metaPage ? undefined : 128,
+  };
+  return useSizeByBreakpoint(sizes) as Spacing;
+};
+
 export function ContentContainer({
   children,
   metaPage,
   ...rest
 }: ContentContainerProps) {
+  const padding = useContentContainerPadding(metaPage);
   const theme = useTheme();
 
   return (
     <FlexBox
       flexDirection="column"
       maxWidth={`${theme.breakpointValues.xl}px`}
-      paddingX={{ base: 20, sm: 64, md: metaPage ? undefined : 128 }}
+      paddingX={padding}
       width="100%"
       {...rest}
     >
