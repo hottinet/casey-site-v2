6c7c98db7de0c4545adf3b64761a1834f233fe7c
Troy Chryssos <troychryssos@gmail.com>
2024-02-26 13:41:26 -0800

    what about scroll

--- src/components/meta/SmNavMenu.tsx
+++ src/components/meta/SmNavMenu.tsx
@@ -77,7 +77,7 @@ export function SmNavMenu({ isOpen, setIsOpen }: SmNavMenuProps) {
         flexDirection="column"
         height="100vh"
         left={isOpen ? 0 : '9999px'}
-        overflow="auto"
+        overflow="scroll"
         position="absolute"
         top={`${top}px`}
         width="100%"
