b3dfa9049ced8201c0925c0b50aa28da207557a0
Casey Bradford <caseyebradford@gmail.com>
2024-04-05 10:25:50 -0700

    adding to culinistas

--- src/pages/culinistas.tsx
+++ src/pages/culinistas.tsx
@@ -4,6 +4,7 @@ import { ContentContainer } from '~/components/ContentContainer';
 import Image from '~/components/Image';
 import { Layout } from '~/components/meta/Layout';
 import { Text } from '~/components/typography/Text';
+import Video from '~/components/videos/Video';
 import { ZOLA_ONBOARDING } from '~/constants/routing';
 import { NAV_BAR_HEIGHT, SM_NAV_BAR_HEIGHT } from '~/constants/styles';
 import { pxToRem } from '~/utils/pxToRem';
@@ -28,10 +29,6 @@ function Culinistas() {
               Connecting users with private chefs
             </Text>
           </FlexBox>
-          <Image
-            alt="Program page on desktop and mobile"
-            src="/Programs/programscover.png"
-          />
         </ContentContainer>
         <ContentContainer gap={32}>
           <GridBox
@@ -49,8 +46,8 @@ function Culinistas() {
                 The Goal
               </Text>
               <Text as="p">
-                The Culinistas was hoping to improve their marketing website as
-                well as bring their analog booking process to the web
+                The Culinistas wanted to improve their marketing website as well
+                as bring their analog booking process to the web.
               </Text>
             </FlexBox>
             <FlexBox flexDirection="column" gap={8}>
@@ -59,8 +56,8 @@ function Culinistas() {
               </Text>
               <Text as="p">
                 I worked directly with the Culinistas Design Director to provide
-                product design expertise, do research, mock up screens and
-                create prototypes.
+                product design expertise, do research, design screens and create
+                prototypes.
               </Text>
             </FlexBox>
           </GridBox>
@@ -75,6 +72,81 @@ function Culinistas() {
             component library and documentation for developers. I also worked to
             ensure designs were responsive and create mobile web versions of
             each screen.
+          </Text>
+          <Video sources={[{ src: '/Culinistas/culinistas_marketing.mp4' }]} />
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <Text as="h2" lineHeight={1.1} variant="headline4">
+            Building a flow for users to book their own services online
+          </Text>
+          <Text as="p" variant="bodySmall">
+            Next, we focused on allowing customers to book their own services
+            directly through the website. The Culinistas offer Weekly Meal Prep,
+            Gatherings and Cooking Classes and we created journeys for each of
+            these booking types.
+          </Text>
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <GridBox
+            gap={40}
+            gridTemplateColumns={{
+              base: '1fr',
+              sm: 'repeat(2, 1fr)',
+            }}
+          >
+            <FlexBox flexDirection="column" gap={24} justifyContent="center">
+              <Text as="h3" lineHeight={1.1} variant="subtitle3">
+                Looking at the existing flow
+              </Text>
+              <Text as="p" variant="bodySmall">
+                The existing flow separated each field onto a new page which
+                made the experience feel very long and there was no sense of
+                progress for the user. The menu selection was also crammed into
+                a small drawer which made it very hard to digest.
+              </Text>
+            </FlexBox>
+            <Image alt="" src="/Culinistas/culinistas_old.png" />
+          </GridBox>
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <FlexBox flexDirection="column" gap={24}>
+            <Text as="p" variant="subtitle3">
+              Mapping out a new user journey
+            </Text>
+            <Text as="p" variant="bodySmall">
+              For our new user flow, we wanted to group related fields into more
+              simple and cohesive steps. After many iterations of this journey,
+              we also decided that we wanted to move the dish selection toward
+              the beginning of the flow, only collecting blocking fields before
+              showing users a menu.
+            </Text>
+          </FlexBox>
+          <Image alt="" src="/Culinistas/culinistas_userjourney.png" />
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <Text as="p" variant="subtitle3">
+            Using wireframes to establish patterns
+          </Text>
+          <GridBox
+            gap={40}
+            gridTemplateColumns={{
+              base: '1fr',
+              sm: 'repeat(3, 1fr)',
+            }}
+          >
+            <Image alt="" src="/Culinistas/wire_1.png" />
+            <Image alt="" src="/Culinistas/wire_2.png" />
+            <Image alt="" src="/Culinistas/wire_3.png" />
+          </GridBox>
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <Text as="p" variant="subtitle3">
+            Booking a gathering
+          </Text>
+        </ContentContainer>
+        <ContentContainer gap={32}>
+          <Text as="p" variant="subtitle3">
+            Adding filtering
           </Text>
         </ContentContainer>
       </FlexBox>
